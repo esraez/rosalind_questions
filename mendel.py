60584025d28565aec623fe001bcc7b3c68a10fae
esraez <76933368+esraez@users.noreply.github.com>
2022-02-15 09:04:20 -0800

    Update mendel.py

--- mendel.py
+++ mendel.py
@@ -5,7 +5,7 @@ Created on Mon Jan 24 10:36:48 2022
 @author: erkol
 """
 #given:Three positive integers k, m, and n, representing a population containing k+m+n organisms: k individuals are homozygous dominant for a factor, m are heterozygous, and n are homozygous recessive.
-#returns:Three positive integers k, m, and n, representing a population containing k+m+n organisms: k individuals are homozygous dominant for a factor, m are heterozygous, and n are homozygous recessive.
+#returns:The probability that two randomly selected mating organisms will produce an individual possessing a dominant allele (and thus displaying the dominant phenotype). Assume that any two organisms can mate.
 k=20
 m=29
 n=21
